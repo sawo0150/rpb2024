d680f0ef53b78f14ef09abf4daf8218f3a4a78bf
sawo0150 <101260689+sawo0150@users.noreply.github.com>
2024-04-04 17:07:19 +0900

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,5 +1,21 @@
 def main():
+    print("Let's implement addition. Type two numbers for x and y.")
 
+    x = int(input("x > "))
+    y = int(input("y > "))
+    
+    print("%d + %d = %d" % (x, y, add(x, y)))    
+    
+# TODO: add() 함수 정의
 def add():
-
+    return x+y
+  
 def divide():
+    if y==0:
+        print("Error: cannot divide by zero.")
+    else:
+        return x/y
+
+  
+if __name__ == "__main__":
+    main()
